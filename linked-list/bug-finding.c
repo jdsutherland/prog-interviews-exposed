98a70744d05783fa96c34c0576424706179f6999
Jeff Sutherland <jeffdsuth@gmail.com>
2020-09-19 09:55:16 -0700

    Fix head accessing free'd memory WIP
    Step 2. check that each line of fn works correctly

--- linked-list/bug-finding.c
+++ linked-list/bug-finding.c
@@ -1,5 +1,6 @@
 // Find bug: supposed to remove the head element from a singly linked list
 void removeHead(ListElement *head) {
+  ListElement *tmp = head->next;
   free(head);
-  head = head->next;
+  head = tmp;
 }
