6e96dcf0127d87106aa2e8800bbb28fa19df35f6
Jeff Sutherland <jeffdsuth@gmail.com>
2020-09-19 10:08:53 -0700

    Check for null ptr
    Step 4. check for common error conditions

--- linked-list/bug-finding.c
+++ linked-list/bug-finding.c
@@ -1,5 +1,7 @@
 // Find bug: supposed to remove the head element from a singly linked list
 void removeHead(ListElement **head) {
+  if (!head || !*head) return;
+
   ListElement *tmp = (*head)->next;
   free(*head);
   *head = tmp;
