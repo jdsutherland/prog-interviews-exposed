7d28926dde5346bf2d74890ea95cdedb44578148
Jeff Sutherland <jeffdsuth@gmail.com>
2020-09-19 10:00:02 -0700

    Fix head mutation: need double ptr b/c pass-by-val
    Step 3. check that the data comes out of the function correctly
    head ptr is passed a copy; mutation req. passing ptr to ptr

--- linked-list/bug-finding.c
+++ linked-list/bug-finding.c
@@ -1,6 +1,6 @@
 // Find bug: supposed to remove the head element from a singly linked list
-void removeHead(ListElement *head) {
-  ListElement *tmp = head->next;
-  free(head);
-  head = tmp;
+void removeHead(ListElement **head) {
+  ListElement *tmp = (*head)->next;
+  free(*head);
+  *head = tmp;
 }
