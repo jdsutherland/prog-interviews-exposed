bd0162da9f34721c7bd4f74c618a56cb9cb6ef4d
Jeff Sutherland <jeffdsuth@gmail.com>
2020-09-23 14:05:03 -0700

    Use array syntax

--- strings-and-arrays/remove_specified.c
+++ strings-and-arrays/remove_specified.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define ASCII 128
 
-// remove chars_to_remove from pp_char
-static void remove_specified(char **pp_char, char *chars_to_remove) {
+// remove chars_to_remove from str
+static void remove_specified(char str[], char *chars_to_remove) {
   bool to_remove[ASCII] = {false};
   for (char *c = chars_to_remove; *c != '\0'; c++) {
     to_remove[*c] = true;
   }
 
   int dst = 0;
-  for (char *p_char = *pp_char; *p_char != '\0'; p_char++) {
-    if (!to_remove[*p_char]) {
-      (*pp_char)[dst++] = *p_char;
+  for (int i = 0; i < strlen(str); i++) {
+    if (!to_remove[str[i]]) {
+      str[dst++] = str[i];
     }
   }
-  (*pp_char)[dst] = '\0';
+  str[dst] = '\0';
 }
 
 int main(void)
 {
   char str[] = "Battle of the Vowels: Hawaii vs. Grozny";
-  char *string = &str[0];
-  remove_specified(&string, "aeiou");
-  printf("%s\n", string);
+  remove_specified(str, "aeiou");
+  printf("%s\n", str);
   return 0;
 }
