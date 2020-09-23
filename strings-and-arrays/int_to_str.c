ac673ffc6caf3fdca654c79c5346034d04185395
Jeff Sutherland <jeffdsuth@gmail.com>
2020-09-23 16:44:15 -0700

    Improve c idiomaticity

--- strings-and-arrays/int_to_str.c
+++ strings-and-arrays/int_to_str.c
@@ -1,7 +1,7 @@
 #include <assert.h>
 #include <string.h>
 
-const int MAX_DIGITS = 10;
+enum { MAX_DIGITS = 10 };
 
 static void reverse_str(char str[], int start, int end) {
   char tmp;
@@ -12,34 +12,38 @@ static void reverse_str(char str[], int start, int end) {
   }
 }
 
-static char *int_to_str(int num) {
-  if (num == 0) return "0";
+static void int_to_str(char dst[], int num) {
+  if (num == 0) {
+    dst[0] = '0';
+    dst[1] = '\0';
+    return;
+  }
 
-  static char buf[MAX_DIGITS];
   int sign = (num < 0) ? -1 : 1;
   int i = 0;
   for (int acc = num * sign; acc > 0; i++) {
-    buf[i] = (acc % 10) + '0';
+    dst[i] = (acc % 10) + '0';
     acc /= 10;
   }
   if (sign == -1) {
-    buf[i] = '-';
-    buf[i+1] = '\0';
+    dst[i] = '-';
+    dst[i+1] = '\0';
   } else {
-    buf[i] = '\0';
+    dst[i] = '\0';
   }
 
-  reverse_str(buf, 0, strlen(buf)-1);
-  return buf;
+  reverse_str(dst, 0, strlen(dst)-1);
 }
 
 int main() {
-  char *zero = int_to_str(0);
-  assert(strcmp(zero, "0") == 0);
+  char buf[MAX_DIGITS];
 
-  char *neg = int_to_str(-137);
-  assert(strcmp(neg, "-137") == 0);
+  int_to_str(buf, 0);
+  assert(strcmp(buf, "0") == 0);
 
-  char *pos = int_to_str(137);
-  assert(strcmp(pos, "137") == 0);
+  int_to_str(buf, -137);
+  assert(strcmp(buf, "-137") == 0);
+
+  int_to_str(buf, 137);
+  assert(strcmp(buf, "137") == 0);
 }
